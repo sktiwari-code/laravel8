0c5ce37ae020f3859835bdb0c6c9f122b2d806c1
sktiwari <sktiwari@lingellearning.com>
2021-07-19 09:20:26 +0000

    some change in file

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about',function(){
-    return view('about');
-});
+Route::view("about",'about');
